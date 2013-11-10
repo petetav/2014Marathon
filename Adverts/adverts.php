0e4f975b76da97a8c306903aa065bea16e04c5ea
Pete Tavener <petethepilot69@gmail.com>
2013-11-10 16:22:38 +0100

    Initial commit

---
+++ Adverts/adverts.php
@@ -0,0 +1,20 @@
+<li class="advert"> 			
+			<a href="http://www.192.com/atoz/business/lingfield-rh7/fish-and-chip-shops/lingfield-traditional-fish-chips/93989be192e01096933bc04b5fb60c7810bb3e98/ml/			
+" alt="fishandchips" width="1268" height="638" target="_blank"><img src="Adverts/FishChips6.jpg" width="120"  height="140"/></a> </li>
+	
+
+<li class="advert"> 			
+	<a href="http://www.bidbury.com" target="_blank"><img src="Adverts/costadelsol.png" width="120"  height="140"></a> </li>
+	
+	
+	<li class="advert">
+  			  <a href="http://www.moverevolution.com" target="_blank"><img src="Adverts/MoveRevolution.jpg" width="120" height="181" /></a> </li>
+  			  
+
+	<li class="advert">  
+		<a href="http://www.oldcagelingfield.co.uk" target="_blank"><img src="Adverts/old_cage.gif" width="120" height="149" 
+		 /></a>		</li>							 
+	
+<li class="advert">  
+		<a href="http://www.howardcundey.com/homek" target="_blank"><img src="Adverts/Howard_Cundey_Ad.jpg" width="120" height="149" 
+		 /></a>		</li>	
